069d8e4f8ac4123c8475c14f8df00435ab576514
Abdul Basit <ab9133711@gmail.com>
2025-09-04 11:55:40 +0500

    fix purchase report and get inventory balance

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,8 +17,8 @@ export default {
         slideDown: 'slideDown o.75s ease-out'
       },
       colors: {
-        primary: '#0c9cdc',
-        primaryHover: '#278eba'
+        primary: '#ff714b',
+        primaryHover: '#c55739'
       }
     },
   },
